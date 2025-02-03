2d3c4aa72e1085adf08785b43d910a91c27c6d9e
Nicolas COCHIN <github@account.rocks>
2025-02-03 08:53:00 +0100

    Update dev-env.zsh

--- files/.zshrc.d/dev-env.zsh
+++ files/.zshrc.d/dev-env.zsh
@@ -9,7 +9,7 @@ _comp_list_vms() {
 }
 
 dev_enter() {
-  bash <(curl -Ls https://raw.githubusercontent.com/nicolascochin/lima-dev/refs/heads/main/create_env.sh) NAME_OF_VM
+  bash <(curl -Ls https://raw.githubusercontent.com/nicolascochin/lima-dev/refs/heads/main/enter_env.sh) NAME_OF_VM
 }
 compdef _comp_list_vms dev_enter
 
