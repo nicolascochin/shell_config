574165f91215c117bdc1c47b757f7376848abef7
Nicolas COCHIN <github@account.rocks>
2025-02-03 12:40:13 +0100

    Add gitconfig

--- install.sh
+++ install.sh
@@ -11,12 +11,30 @@ if [ ! -d "$FILES_DIR" ]; then
   exit 1
 fi
 
+# Git config
+while [[ -z "${GITHUB_USER}" ]]; do
+  read -p "Entrez votre nom d'utilisateur GitHub : " GITHUB_USER
+done
+while [[ -z "${GITHUB_EMAIL}" ]]; do
+  read -p "Entrez votre email GitHub : " GITHUB_EMAIL
+done
+
+cat <<EOF > ~/.git-credentials
+[user]
+  name = ${GITHUB_USER}
+  email = ${GITHUB_EMAIL}
+EOF
+chmod 600 ~/.git-credentials
+
 echo "Install OMZ"
-# Install OMZ
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# Install OMZ plugins & theme
-git clone https://github.com/mattmc3/zshrc.d $HOME/.oh-my-zsh/custom/plugins/zshrc.d
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+if [[ ! -d ~/.oh-my-zsh ]]
+then
+  # Install OMZ
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  # Install OMZ plugins & theme
+  git clone https://github.com/mattmc3/zshrc.d $HOME/.oh-my-zsh/custom/plugins/zshrc.d
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+fi 
 
 echo "Create links"
 # Loop through all files in the files directory
