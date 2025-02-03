f009d58cf23d3a92fefc3094572ecbefddf9de2c
Nicolas COCHIN <github@account.rocks>
2025-02-03 09:01:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,13 @@ if [ ! -d "$FILES_DIR" ]; then
   exit 1
 fi
 
+echo "Install OMZ"
+# Install OMZ
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install OMZ plugins & theme
+git clone https://github.com/mattmc3/zshrc.d $HOME/.oh-my-zsh/custom/plugins/zshrc.d
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+
 echo "Create links"
 # Loop through all files in the files directory
 shopt -s dotglob
